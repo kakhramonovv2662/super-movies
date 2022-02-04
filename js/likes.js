51141bd4fa731435af5d0bfbab571ef9d1965738
kakhramonovv2662 <qahramonovismoil860@gmail.com>
2022-02-04 16:52:20 +0500

    finished movies site

--- js/likes.js
+++ js/likes.js
@@ -1,5 +1,8 @@
-let bookmarkWrapper = findElement('.bookmark-wrapper', document)
-let bookmarkBtn = findElement('.bookmark_btn')
+let bookmarkWrapper = findElement(".bookmark-wrapper", document);
+let bookmarkBtn = findElement(".bookmark_btn");
+let elList = findElement(".bookmark-wrapper");
+let bookmarkTemplate = findElement(".bookmark-template", document).content;
+
 // let allBookmark = []
 
 // let createBookmarkItem = (movie) => {
@@ -9,92 +12,80 @@ let bookmarkBtn = findElement('.bookmark_btn')
 //     let bookmarkTitle = findElement('.bookmarked-title', templateClone)
 //     let bookmarkDeleteBtn = findElement('.js-bookmarked-btn', templateClone)
 
-
 //     bookmarkWrapper.append(templateClone)
 // }
 
 // createBookmarkItem()
 
-
 let handleBookmarkBtn = () => {
-    bookmarkWrapper.classList.toggle('bookmark-list')
-}
+  bookmarkWrapper.classList.toggle("bookmark-list");
+};
 
-bookmarkBtn.addEventListener ('click', handleBookmarkBtn)
+bookmarkBtn.addEventListener("click", handleBookmarkBtn);
 
 // moviesList.addEventListener('click', handleClickList)
 
-let localData = JSON.parse(localStorage.getItem('movies'))
-var bookmarks = localData || []
+let localData = JSON.parse(localStorage.getItem("movies"));
+var bookmarks = localData || [];
 
-
-let i = localData?.at(-1).id || 0
-
-let elList = findElement('.bookmark-wrapper')
-let bookmarkTemplate = findElement('.bookmark-template', document).content
+let i = localData?.at(-1).id || 0;
 
 function handleClickItem(evt) {
-   if (evt.target.matches('.js-bookmarked-btn')) {
-    deleteMovie(evt.currentTarget.dataset.id)
-   }
-    
+  if (evt.target.matches(".js-bookmarked-btn")) {
+    deleteMovie(evt.currentTarget.dataset.id);
+  }
 }
 
 function createItem(element) {
-    let cloneTemplate = bookmarkTemplate.cloneNode(true)
+  let cloneTemplate = bookmarkTemplate.cloneNode(true);
 
-    let elItem = findElement('.bookmark-item', cloneTemplate)
-    let bookmarkedTitle = findElement('.bookmarked-title', cloneTemplate)
-    let bookmarkedDeleteBtn = findElement('.js-delete-btn', cloneTemplate)
-    
-    bookmarkedTitle.textContent = element.title 
-    elItem.dataset.id = element.id
+  let elItem = findElement(".bookmark-item", cloneTemplate);
+  let bookmarkedTitle = findElement(".bookmarked-title", cloneTemplate);
+  let bookmarkedDeleteBtn = findElement(".js-delete-btn", cloneTemplate);
 
-    elItem.addEventListener('click', handleClickItem)
-    // bookmarkedDeleteBtn.addEventListener ('click', deleteMovie)
+  bookmarkedTitle.textContent = element.title;
+  bookmarkedDeleteBtn.dataset.id = element.imdbId;
+  // elItem.dataset.id = element.id
 
-    elList.append(cloneTemplate)
+  // elItem.addEventListener('click', handleClickItem)
+  bookmarkedDeleteBtn.addEventListener("click", deleteMovie);
+
+  elList.append(cloneTemplate);
 }
 
-function deleteMovie (id) {
-    let deletedMovies = bookmarks.filter((movie) => movie.id !== Number(id))
-        console.log(deletedMovies);
+function deleteMovie(evt) {
+  let deletedMovies = bookmarks.filter(
+    (movie) => movie.imdbId !== evt.target.dataset.id
+  );
+  console.log(deletedMovies);
 
-        bookmarks = deletedMovies
-        localStorage.setItem('movies', JSON.stringify(bookmarks))
+  bookmarks = deletedMovies;
+  localStorage.setItem("movies", JSON.stringify(bookmarks));
 
-        elList.innerHTML = null
-        deletedMovies.forEach(element => {
-          createItem(element);
-        })
+  elList.innerHTML = null;
+  deletedMovies.forEach((element) => {
+    createItem(element);
+  });
 }
 
 function handleLikeBtn(evt) {
-  
-    if (evt.target.matches('.js-save-btn')) {
-      
-      let foundMovie = MOVIES.find(
-        (movie) => movie.imdbId === evt.target.dataset.id
-      );
-  
-  
-        let newTodo = {
-          id: ++i,
-          title: foundMovie.title,
-        }
-  
-        bookmarks.push(newTodo)
+  if (evt.target.matches(".js-save-btn")) {
+    let foundMovie = MOVIES.find(
+      (movie) => movie.imdbId === evt.target.dataset.id
+    );
 
-        elList.innerHTML = null
-  
-        bookmarks.forEach(element => {
-          createItem(element);
-        })
-    }
+    bookmarks.push(foundMovie);
 
-    localStorage.setItem('movies', JSON.stringify(bookmarks))
+    elList.innerHTML = null;
+
+    bookmarks.forEach((element) => {
+      createItem(element);
+    });
   }
 
-  bookmarks.forEach(title => {
-    createItem(title);
-  })
+  localStorage.setItem("movies", JSON.stringify(bookmarks));
+}
+
+bookmarks.forEach((title) => {
+  createItem(title);
+});
